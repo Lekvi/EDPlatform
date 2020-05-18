acb4aa2158079e8e4d5722df27fc57d00f59265e
Nikita Dychek <Dinitius@gmail.com>
2020-05-18 20:37:35 +0300

    New version

--- src/components/app/chart.vue
+++ src/components/app/chart.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="info-block col xl9 l9 m9 s9 z-depth-1">
-    <h3 class="info-block-title black-text">Заголовок</h3>
-    <div class="small">
-      <line-chart :chart-data="datacollection"></line-chart>
-      <button @click="fillData()">Randomize</button>
-    </div>
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
   </div>
 </template>
+
 <script>
 import LineChart from "./LineChart.js";
 
@@ -46,22 +44,8 @@ export default {
   }
 };
 </script>
-<style>
-.info-block {
-  margin: 0 !important;
-  padding: 25px !important;
-  margin: 0;
-  border-radius: 10px;
-  min-height: 500px !important;
-  box-sizing: border-box;
-}
-.info-block-title {
-  margin: 0;
-  color: black;
-  font-size: 28px;
-  font-weight: bold;
-}
 
+<style>
 .small {
   max-width: 600px;
   margin: 150px auto;
